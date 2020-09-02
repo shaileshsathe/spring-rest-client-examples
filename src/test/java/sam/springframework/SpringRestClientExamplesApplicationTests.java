b73a0280dafabd1bee984043bfd4e6a5bffe8dc8
Shailesh Sathe <shaileshsathe512@gmail.com>
2020-09-02 19:08:48 +0530

    Initial Commit

---
+++ src/test/java/sam/springframework/SpringRestClientExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package sam.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestClientExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
